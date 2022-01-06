019a65812d2fbea176b9a1ca31645c605bf0ec35
kamiada <kamiada528@gmail.com>
2022-01-06 14:33:47 +0000

    nana

---
+++ App/Actions/actions.tsx
@@ -0,0 +1,10 @@
+export const ADD_BOOK = 'ADD_BOOK';
+export const REMOVE_BOOK = 'REMOVE_BOOK';
+
+export const addBook = (title : string) => {
+
+}
+
+export const removeBook = (book: string) => {
+
+}
--- App/BookList.tsx
+++ App/BookList.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
+const d = new Date();
+let year = d.getFullYear();
+
+
 const BookList = () => {
   return (
     <View style={styles.container}>
-      <Text>Book list Screen</Text>
+      <Text>Books you read in {year}</Text>
+        {booklist 0 && {
+
+        }}
     </View>
   );
 }
