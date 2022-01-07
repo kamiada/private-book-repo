8c89e2f3b5b6c9e708592710497aac57a5adfb50
kamiada <kamiada528@gmail.com>
2022-01-07 12:51:51 +0000

    aaah learning

--- App/BookList.tsx
+++ App/BookList.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { FC, ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
 const d = new Date();
 let year = d.getFullYear();
 
+type myProps = {
+  booklist: Array<String>;
+}
 
-const BookList = () => {
+
+const BookList: FC<myProps> = ({booklist}): ReactElement => {
   return (
     <View style={styles.container}>
       <Text>Books you read in {year}</Text>
-        {booklist 0 && {
+        {booklist  && {
 
         }}
     </View>
